6cde030a2b33c02028a05cac1e32c36dc41c1a90
Jessica <119410030+jsscmnhn@users.noreply.github.com>
2025-05-13 09:01:35 +0200

    stashed

--- util/shadowingfunctions.py
+++ util/shadowingfunctions.py
@@ -252,9 +252,6 @@ def shadowingfunctionglobalradiation_3d(a, amaxvalue, azimuth, altitude, scale):
 
     return sh
 
-# # @jit(nopython=True)
-# # @profile
-
 
 # @profile
 def shadowingfunction_20_cupy(a, vegdem, vegdem2, azimuth, altitude, scale, amaxvalue, bush):
@@ -883,3 +880,143 @@ def shadowingfunction_20_old(a, vegdem, vegdem2, azimuth, altitude, scale, amaxv
     dscos = np.abs((1. / cosazimuth))
     tanaltitudebyscale = np.tan(altitude) / scale
     index = 1
+
+# kind of faster shadowing_20 version
+def shadowingfunction_20v2(a, vegdem, vegdem2, azimuth, altitude, scale, amaxvalue, trunkcheck, bush, forsvf):
+
+    # This function casts shadows on buildings and vegetation units.
+    # New capability to deal with pergolas 20210827
+
+    # conversion
+    degrees = np.pi/180.
+    azimuth = azimuth * degrees
+    altitude = altitude * degrees
+
+    # measure the size of grid
+    sizex = a.shape[0]
+    sizey = a.shape[1]
+
+    # progressbar for svf plugin
+    if forsvf == 0:
+        barstep = np.max([sizex, sizey])
+        total = 100. / barstep
+        # dlg.progressBar.setRange(0, barstep)
+        # dlg.progressBar.setValue(0)
+
+    # initialise parameters
+    dx = 0.
+    dy = 0.
+    dz = 0.
+    temp = np.zeros((sizex, sizey))
+    tempvegdem = np.zeros((sizex, sizey))
+    tempvegdem2 = np.zeros((sizex, sizey))
+    templastfabovea = np.zeros((sizex, sizey))
+    templastgabovea = np.zeros((sizex, sizey))
+    bushplant = bush > 1.
+    sh = np.zeros((sizex, sizey)) #shadows from buildings
+    vbshvegsh = np.zeros((sizex, sizey)) #vegetation blocking buildings
+    vegsh = np.add(np.zeros((sizex, sizey)), bushplant, dtype=float) #vegetation shadow
+    f = a
+
+    pibyfour = np.pi / 4.
+    threetimespibyfour = 3. * pibyfour
+    fivetimespibyfour = 5.* pibyfour
+    seventimespibyfour = 7. * pibyfour
+    sinazimuth = np.sin(azimuth)
+    cosazimuth = np.cos(azimuth)
+    tanazimuth = np.tan(azimuth)
+    signsinazimuth = np.sign(sinazimuth)
+    signcosazimuth = np.sign(cosazimuth)
+    dssin = np.abs((1./sinazimuth))
+    dscos = np.abs((1./cosazimuth))
+    tanaltitudebyscale = np.tan(altitude) / scale
+    # index = 1 6
+    index = 0
+    isVert = ((pibyfour <= azimuth) and (azimuth < threetimespibyfour) or (fivetimespibyfour <= azimuth) and (
+                azimuth < seventimespibyfour))
+    if isVert:
+        ds = dssin
+    else:
+        ds = dscos
+
+    # preva = a + ds
+    dzprev = 0
+
+    # main loop
+    while (amaxvalue >= dz) and (np.abs(dx) < sizex) and (np.abs(dy) < sizey):
+        if forsvf == 0:
+            print(int(index * total))  # dlg.progressBar.setValue(index)
+        if isVert:
+            dy = signsinazimuth * index
+            dx = -1. * signcosazimuth * np.abs(np.round(index / tanazimuth))
+        else:
+            dy = signsinazimuth * np.abs(np.round(index * tanazimuth))
+            dx = -1. * signcosazimuth * index
+        # note: dx and dy represent absolute values while ds is an incremental value
+        dz = (ds * index) * tanaltitudebyscale
+        tempvegdem[0:sizex, 0:sizey] = 0.
+        tempvegdem2[0:sizex, 0:sizey] = 0.
+        temp[0:sizex, 0:sizey] = 0.
+
+        templastfabovea[0:sizex, 0:sizey] = 0.
+        templastgabovea[0:sizex, 0:sizey] = 0.
+
+        absdx = np.abs(dx)
+        absdy = np.abs(dy)
+        xc1 = int((dx + absdx) / 2.)
+        xc2 = int(sizex + (dx - absdx) / 2.)
+        yc1 = int((dy + absdy) / 2.)
+        yc2 = int(sizey + (dy - absdy) / 2.)
+        xp1 = int(-((dx - absdx) / 2.))
+        xp2 = int(sizex - (dx + absdx) / 2.)
+        yp1 = int(-((dy - absdy) / 2.))
+        yp2 = int(sizey - (dy + absdy) / 2.)
+        isTrunk = trunkcheck >= dz
+        # print(dy, dx, dz)
+        # print(f' xc1: {xc1}; xc2: {xc2}, yc1:  {yc1}, yc2: {yc2}, xp1: {xp1}, xp2: {xp2}, yp1: {yp1}, yp2: {yp2} ')
+        tempvegdem[xp1:xp2, yp1:yp2] = vegdem[xc1:xc2, yc1:yc2] - dz
+        temp[xp1:xp2, yp1:yp2] = a[xc1:xc2, yc1:yc2] - dz
+
+        f = np.fmax(f, temp)  # Moving building shadow
+        sh[(f > a)] = 1.
+        sh[(f <= a)] = 0.
+        fabovea = tempvegdem > a  # vegdem above DEM
+
+        templastfabovea[xp1:xp2, yp1:yp2] = vegdem[xc1:xc2, yc1:yc2] - dzprev
+        lastfabovea = templastfabovea > a
+
+        if isTrunk:
+            tempvegdem2[xp1:xp2, yp1:yp2] = vegdem2[xc1:xc2, yc1:yc2] - dz
+            gabovea = tempvegdem2 > a  # vegdem2 above DEM
+
+            # new pergola condition
+            templastgabovea[xp1:xp2, yp1:yp2] = vegdem2[xc1:xc2, yc1:yc2]- dzprev
+            lastgabovea = templastgabovea > a
+
+            vegsh2 = np.add(np.add(np.add(fabovea, gabovea, dtype=float), lastfabovea, dtype=float), lastgabovea,
+                            dtype=float)
+
+            # Apply the zeroing condition
+            vegsh2[vegsh2 == 4] = 0.
+            vegsh2[vegsh2 > 0] = 1.
+        else:
+            vegsh2 = (fabovea | lastfabovea).astype(float)
+        vegsh = np.fmax(vegsh, vegsh2)
+
+
+
+        vegsh[(vegsh * sh > 0.)] = 0.
+        vbshvegsh = vegsh + vbshvegsh  # removing shadows 'behind' buildings
+
+        dzprev = dz
+        index += 1.
+
+    sh = 1.-sh
+    vbshvegsh[(vbshvegsh > 0.)] = 1.
+    vbshvegsh = vbshvegsh-vegsh
+    vegsh = 1.-vegsh
+    vbshvegsh = 1.-vbshvegsh
+
+    shadowresult = {'sh': sh, 'vegsh': vegsh, 'vbshvegsh': vbshvegsh}
+
+    return shadowresult
